12ba0164cc61de157bb109dad3d44c45d073f6f6
Andrew Casper <andrew@argosindustries.com>
2019-10-21 21:22:18 -0500

    Building up the entrypoint.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-echo "Hello World!"
-echo $1
+SECRET_TOKEN=$1
+REPO=$2
+CONFIG=$3
+PROJECT=$4
+
+EMBUILD=/ses/bin/emBuild
+
+git clone https://${SECRET_TOKEN}@github.com/${REPO} /repo
+
+${EMBUILD} -verbose -config ${CONFIG} -batch /repo/${PROJECT}

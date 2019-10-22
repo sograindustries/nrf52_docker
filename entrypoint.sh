5ce22fe920d1d36b7db21f493bdedbf4a46f4ac5
Andrew Casper <andrew@argosindustries.com>
2019-10-21 22:20:42 -0500

    Working locally.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,14 @@ SECRET_TOKEN=$1
 REPO=$2
 CONFIG=$3
 PROJECT=$4
+ARTIFACT=$5
+MAPPED_DIR=$6
 
 EMBUILD=/ses/bin/emBuild
 
 git clone https://${SECRET_TOKEN}@github.com/${REPO} /repo
 
 ${EMBUILD} -verbose -config ${CONFIG} -batch /repo/${PROJECT}
+
+cp $ARTIFACT $MAPPED_DIR
+

5b1e84ad618e264c0bc4591920aa8240bc1cbb5f
Andrew Casper <andrew@argosindustries.com>
2019-10-30 09:58:34 -0500

    Add a post build script and pip install.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@ CONFIG=$4
 PROJECT=$5
 ARTIFACT=$6
 MAPPED_DIR=$7
+POST_BUILD_SCRIPT=$8
 
 EMBUILD=/ses/bin/emBuild
 
@@ -15,7 +16,13 @@ git clone https://${SECRET_TOKEN}@github.com/${REPO} /repo
 cd /repo
 git checkout ${COMMIT}
 
+pip install -r requirements.txt
+
 ${EMBUILD} -verbose -config ${CONFIG} -batch /repo/${PROJECT}
+
+if test -f "$POST_BUILD_SCRIPT"; then
+    ${POST_BUILD_SCRIPT}
+fi
 
 cp $ARTIFACT $MAPPED_DIR
 
